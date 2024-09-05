886e0aa446ec629e215bed8084eef9f772442351
andyotani <andidipo6@gmail.com>
2024-09-05 09:50:18 +0700

    commit pertama saya lho

--- MyFirstJavaGithub01.java
+++ MyFirstJavaGithub01.java
@@ -1,5 +1,5 @@
 public class MyFirstJavaGithub01 {
     public static void main(String[] args) {
-        System.out.println("Hello from Github");
+        System.out.println("Hello from Githubb");
     }
 }
